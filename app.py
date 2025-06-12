116b3f43b8961ece7871f23641fac92d584dbfb1
mohamed ahmed awees <119456950+moawees@users.noreply.github.com>
2025-06-12 16:46:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
 # Run the application if this script is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000) 
